b57e003aff838ccca2ef762efd64ea2a2d717e04
Jeroen Breuer <jbreuer@gmail.com>
2016-02-24 13:44:40 +0100

    Use runtime cache for the content finder.

--- Sources/Umbraco.Extensions/ContentFinders/MultilingualContentFinder.cs
+++ Sources/Umbraco.Extensions/ContentFinders/MultilingualContentFinder.cs
@@ -1,9 +1,11 @@
 ﻿namespace Umbraco.Extensions.ContentFinders
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
 
     using Umbraco.Core;
+    using Umbraco.Core.Cache;
     using Umbraco.Core.Logging;
     using Umbraco.Extensions.Models;
     using Umbraco.Web;
@@ -39,18 +41,33 @@
                 {
                     var helper = new UmbracoHelper(UmbracoContext.Current);
 
-                    // Get the current url without querystring.
-                    var url = this.RemoveQueryFromUrl(contentRequest.Uri.ToString()).EnsureEndsWith("/");
+                    var urls = UmbracoContext.Current.Application.ApplicationCache.RuntimeCache.GetCacheItem<List<Tuple<int, string>>>(
+                        "MultilingualContentFinder-Urls",
+                        () =>
+                        {
+                            // Get all the nodes in the website.
+                            var allNodes = helper.TypedContentAtRoot().DescendantsOrSelf<UmbMaster>().ToList();
 
-                    // Get all the nodes in the website. This could be bad for performance so should be cached somehow.
-                    var allNodes = helper.TypedContentAtRoot().DescendantsOrSelf<UmbMaster>();
-                    
-                    // See if we can find a node that matched the url. When we check for .Url the UrlProvider will return the correct multilingual url. 
-                    var content = allNodes.FirstOrDefault(x => x.Url.InvariantEquals(url));
+                            // Get all the urls in the website.
+                            return allNodes.Select(x => new Tuple<int, string>(x.Id, x.Url)).ToList();
+                        });
 
-                    if (content != null)
+                    if (urls.Any())
                     {
-                        contentRequest.PublishedContent = content;
+                        // Get the current url without querystring.
+                        var url = this.RemoveQueryFromUrl(contentRequest.Uri.ToString()).EnsureEndsWith("/");
+
+                        var currentUrlItem = urls.FirstOrDefault(x => url.Equals(x.Item2));
+
+                        if (currentUrlItem != null)
+                        {
+                            var contentItem = UmbracoContext.Current.ContentCache.GetById(currentUrlItem.Item1);
+
+                            if (contentItem != null)
+                            {
+                                contentRequest.PublishedContent = contentItem;
+                            }
+                        }
                     }
                 }
             }
--- Sources/Umbraco.Extensions/Events/UmbracoEvents.cs
+++ Sources/Umbraco.Extensions/Events/UmbracoEvents.cs
@@ -6,10 +6,13 @@
 //   The umbraco events.
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
-
 namespace Umbraco.Extensions.Events
 {
     using Umbraco.Core;
+    using Umbraco.Core.Events;
+    using Umbraco.Core.Models;
+    using Umbraco.Core.Publishing;
+    using Umbraco.Core.Services;
     using Umbraco.Extensions.ContentFinders;
     using Umbraco.Extensions.UrlProviders;
     using Umbraco.Web.Routing;
@@ -28,10 +31,15 @@ namespace Umbraco.Extensions.Events
         /// <param name="applicationContext">
         /// The application context.
         /// </param>
-        protected override void ApplicationStarting(
-            UmbracoApplicationBase umbracoApplication, 
-            ApplicationContext applicationContext)
+        protected override void ApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
         {
+            // Events.
+            ContentService.Published += this.ContentPublished;
+            ContentService.UnPublished += this.ContentUnpublished;
+            ContentService.Moved += this.ContentMoved;
+            ContentService.Trashed += this.ContentTrashed;
+            ContentService.Deleted += this.ContentDeleted;
+
             // With the url providers we can change node urls.
             UrlProviderResolver.Current.InsertTypeBefore<DefaultUrlProvider, MultilingualUrlProvider>();
 
@@ -44,6 +52,37 @@ namespace Umbraco.Extensions.Events
 
             // Remove the ContentFinderByNiceUrl because our MultilingualContentFinder should find all the content.
             ContentFinderResolver.Current.RemoveType<ContentFinderByNiceUrl>();
+        }
+
+        private void ContentPublished(IPublishingStrategy sender, PublishEventArgs<IContent> e)
+        {
+            this.ClearCache();
+        }
+
+        private void ContentDeleted(IContentService sender, DeleteEventArgs<IContent> e)
+        {
+            this.ClearCache();
+        }
+
+        private void ContentTrashed(IContentService sender, MoveEventArgs<IContent> e)
+        {
+            this.ClearCache();
+        }
+
+        private void ContentMoved(IContentService sender, MoveEventArgs<IContent> e)
+        {
+            this.ClearCache();
+        }
+
+        private void ContentUnpublished(IPublishingStrategy sender, PublishEventArgs<IContent> e)
+        {
+            this.ClearCache();
+        }
+
+        private void ClearCache()
+        {
+            // After content has been updated clear content finder cache.
+            ApplicationContext.Current.ApplicationCache.RuntimeCache.ClearCacheByKeySearch("MultilingualContentFinder");
         }
     }
 }
