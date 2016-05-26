2fc1cba1259916881091cdf585efa42395e07f51
Jeroen Breuer <jbreuer@gmail.com>
2016-05-26 11:34:09 +0200

    Media selected with the media picker uses a multilingual alt property.

---
+++ Sources/Umbraco.Extensions/Models/Image.cs
@@ -0,0 +1,25 @@
+﻿namespace Umbraco.Extensions.Models
+{
+    using Our.Umbraco.Vorto.Extensions;
+
+    using Umbraco.ModelsBuilder;
+
+    /// <summary>
+    /// The umb news item.
+    /// </summary>
+    public partial class Image
+    {
+        /// <summary>
+        /// Gets the alt text. If it's empty return the image name.
+        /// </summary>
+        [ImplementPropertyType("alt")]
+        public string Alt
+        {
+            get
+            {
+                var alt = this.GetVortoValue<string>("alt");
+                return !string.IsNullOrEmpty(alt) ? alt : this.Name;
+            }
+        }
+    }
+}
